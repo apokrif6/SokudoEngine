b3b9f1f7aafc5fe6b4c13a7f4e948d9b9c58dbb6
apokrif6 <j.berenchan@gmail.com>
2025-09-24 17:46:35 +0200

    Create Logger::log function version with std::puts. Templated Logger::log with arguments now automatically add \n at end

--- source/core/tools/Logger.h
+++ source/core/tools/Logger.h
@@ -4,11 +4,22 @@
 class Logger
 {
   public:
-    template <typename... Args> static void log(unsigned int logLevel, Args... args)
+    template <typename... Args>
+    static void log(unsigned int logLevel, Args... args)
     {
         if (logLevel <= currentLogLevel)
         {
             std::printf(args...);
+            std::printf("\n");
+            std::fflush(stdout);
+        }
+    }
+
+    static void log(unsigned int logLevel, const char* message)
+    {
+        if (logLevel <= currentLogLevel)
+        {
+            std::puts(message);
             std::fflush(stdout);
         }
     }
